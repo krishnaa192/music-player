54f3b4990f567a1b9dd970dc4c67f7f979303219
Krishna <krishna.upadhyayyyy@gmail.com>
2022-07-08 22:52:02 +0530

    Commit as branch

---
+++ music/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+	#/music/
+   path('', views.IndexView.as_view(), name='index'),
+
+    #/music/{album_id}/
+    path('detail/<str:pk>/', views.DetailView.as_view(), name='detail'),
+
+    #/music/album/add
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+
+]
